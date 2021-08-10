5e8eabb12dd6fe1d54e08da34a81d213a3e1156b
David Zhao <david@davidzhao.com>
2021-08-09 23:46:29 -0700

    fixed tests with field change

--- src/WebhookReceiver.test.ts
+++ src/WebhookReceiver.test.ts
@@ -6,8 +6,8 @@ const testApiKey = 'abcdefg';
 const testSecret = 'abababa';
 
 describe('webhook receiver', () => {
-  const body = '{"type":"room_started", "room":{"sid":"RM_TkVjUvAqgzKz", "name":"mytestroom", "emptyTimeout":300, "creationTime":"1628545903", "turnPassword":"ICkSr2rEeslkN6e9bXL4Ji5zzMD5Z7zzr6ulOaxMj6N", "enabledCodecs":[{"mime":"audio/opus"}, {"mime":"video/VP8"}]}}';
-  const sha = 'GtHiN0xN5VL9eINaNRBBqBv6uwcN6paHVeW0joe0iFY=';
+  const body = '{"event":"room_started", "room":{"sid":"RM_TkVjUvAqgzKz", "name":"mytestroom", "emptyTimeout":300, "creationTime":"1628545903", "turnPassword":"ICkSr2rEeslkN6e9bXL4Ji5zzMD5Z7zzr6ulOaxMj6N", "enabledCodecs":[{"mime":"audio/opus"}, {"mime":"video/VP8"}]}}';
+  const sha = 'CoEQz1chqJ9bnZRcORddjplkvpjmPujmLTR42DbefYI=';
   const t = new AccessToken(testApiKey, testSecret);
   t.sha256 = sha;
   const token = t.toJwt();
@@ -17,7 +17,7 @@ describe('webhook receiver', () => {
     const event = receiver.receive(body, token);
     expect(event).toBeTruthy();
     expect(event.room?.name).toBe('mytestroom');
-    expect(event.type).toBe('room_started');
+    expect(event.event).toBe('room_started');
   });
 });
 

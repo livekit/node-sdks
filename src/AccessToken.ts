c86b8254e04c813230e534ee546be8688b3b6caf
Noah Tye <hi@noahtye.com>
2023-03-27 13:49:13 -0700

    add error log when running in the browser

--- src/AccessToken.ts
+++ src/AccessToken.ts
@@ -53,6 +53,11 @@ export class AccessToken {
     }
     if (!apiKey || !apiSecret) {
       throw Error('api-key and api-secret must be set');
+    } else if (typeof document !== 'undefined') {
+      // check against document rather than window because deno provides window
+      console.error("You should not include your API secret in your web client bundle.\n\n" +
+        "Your web client should request a token from your backend server which should then use " +
+        "the API secret to generate a token. See https://docs.livekit.io/docs/client-sdk/js");
     }
 
     this.apiKey = apiKey;

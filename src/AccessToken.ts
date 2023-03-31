f0f274438037c130c319b0b831b6a0c5f4c0b81b
Noah Tye <hi@noahtye.com>
2023-03-30 23:21:43 -0700

    Print error when creating a token inside a browser (#65)

--- src/AccessToken.ts
+++ src/AccessToken.ts
@@ -53,6 +53,13 @@ export class AccessToken {
     }
     if (!apiKey || !apiSecret) {
       throw Error('api-key and api-secret must be set');
+    } else if (typeof document !== 'undefined') {
+      // check against document rather than window because deno provides window
+      console.error(
+        'You should not include your API secret in your web client bundle.\n\n' +
+          'Your web client should request a token from your backend server which should then use ' +
+          'the API secret to generate a token. See https://docs.livekit.io/client/connect/',
+      );
     }
 
     this.apiKey = apiKey;

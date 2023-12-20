43f540d0950dff8673ba1689125662a766a8098e
lukasIO <mail@lukasseiler.de>
2023-12-20 13:55:34 +0100

    Make `WebhookEvent` names type safe (#125)
    * Make WebhookEventNames type safe
    
    * Create modern-schools-hug.md

--- src/WebhookReceiver.ts
+++ src/WebhookReceiver.ts
@@ -1,7 +1,42 @@
+import { BinaryReadOptions, JsonValue, JsonReadOptions } from '@bufbuild/protobuf';
 import { TokenVerifier } from './AccessToken.js';
-import { WebhookEvent } from './proto/livekit_webhook_pb.js';
+import { WebhookEvent as ProtoWebhookEvent } from './proto/livekit_webhook_pb.js';
 
 export const authorizeHeader = 'Authorize';
+
+export class WebhookEvent extends ProtoWebhookEvent {
+  event: WebhookEventNames = '';
+
+  static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): WebhookEvent {
+    return new WebhookEvent().fromBinary(bytes, options);
+  }
+
+  static fromJson(jsonValue: JsonValue, options?: Partial<JsonReadOptions>): WebhookEvent {
+    return new WebhookEvent().fromJson(jsonValue, options);
+  }
+
+  static fromJsonString(jsonString: string, options?: Partial<JsonReadOptions>): WebhookEvent {
+    return new WebhookEvent().fromJsonString(jsonString, options);
+  }
+}
+
+export type WebhookEventNames =
+  | 'room_started'
+  | 'room_finished'
+  | 'participant_joined'
+  | 'participant_left'
+  | 'track_published'
+  | 'track_unpublished'
+  | 'egress_started'
+  | 'egress_updated'
+  | 'egress_ended'
+  | 'ingress_started'
+  | 'ingress_ended'
+  /**
+   * @internal
+   * @note only used as a default value, not a valid webhook event
+   */
+  | '';
 
 export class WebhookReceiver {
   private verifier: TokenVerifier;

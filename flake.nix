040f40f0bcd10b43fc224070c29d9376b520e7d1
aoife cassidy <aoife@cassidy.sh>
2024-03-29 08:52:01 +0300

    flake: init

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, utils, nixpkgs }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = (import nixpkgs) {
+        inherit system;
+      };
+
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nodePackages.pnpm
+            cargo
+            rustc
+            xorg.libXext
+            xorg.libX11
+            libGL
+          ];
+        };
+      }
+    );
+}

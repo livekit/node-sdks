22991bca7a39c6025809f047ff79296861d6a868
aoife cassidy <aoife@livekit.io>
2024-06-07 18:27:36 +0300

    examples/basic_room: add a complete example

--- examples/basic_room/index.ts
+++ examples/basic_room/index.ts
@@ -1,6 +1,31 @@
-import { Room } from '@livekit/rtc-node';
+import { readFileSync } from 'node:fs';
+import {
+  AudioFrame,
+  AudioSource,
+  LocalAudioTrack,
+  Room,
+  TrackPublishOptions,
+  TrackSource,
+} from '@livekit/rtc-node';
+import { AccessToken } from 'livekit-server-sdk';
 
+// create access token from API credentials
+const token = await new AccessToken(process.env.LIVEKIT_API_KEY, process.env.LIVEKIT_API_SECRET).toJwt()
+
+// set up room
 const room = new Room();
-room.connect('ws://localhost:7880', 'my-token');
+await room.connect(process.env.LIVEKIT_URL, token, { autoSubscribe: true, dynacast: true });
+console.log('connected to room', room);
 
-console.log('room', room);
+// set up audio track
+const source = new AudioSource(16000, 1);
+const track = LocalAudioTrack.createAudioTrack('audio', source);
+const options = new TrackPublishOptions();
+options.source = TrackSource.SOURCE_MICROPHONE;
+
+// read file into Uint16Array
+const sample = readFileSync('../speex.wav');
+var buffer = new Uint16Array(sample.buffer);
+
+await room.localParticipant.publishTrack(track, options);
+await source.captureFrame(new AudioFrame(buffer, 16000, 1, buffer.byteLength / 2))

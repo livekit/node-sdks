d578d855ca5b82a7c05f1421e4e9d5f1f7098d62
aoife cassidy <aoife@livekit.io>
2024-06-08 18:01:28 +0300

    examples/basic_room: make it work when unbugged

--- examples/basic_room/index.ts
+++ examples/basic_room/index.ts
@@ -11,11 +11,19 @@ import { AccessToken } from 'livekit-server-sdk';
 import { join } from 'node:path';
 
 // create access token from API credentials
-const token = await new AccessToken(process.env.LIVEKIT_API_KEY, process.env.LIVEKIT_API_SECRET).toJwt()
+const token = new AccessToken(process.env.LIVEKIT_API_KEY, process.env.LIVEKIT_API_SECRET, {
+  identity: 'example-participant',
+})
+token.addGrant({
+  room: 'example-room',
+  roomJoin: true,
+  roomCreate: true,
+});
+const jwt = await token.toJwt();
 
 // set up room
 const room = new Room();
-await room.connect(process.env.LIVEKIT_URL, token, { autoSubscribe: true, dynacast: true });
+await room.connect(process.env.LIVEKIT_URL, jwt, { autoSubscribe: true, dynacast: true });
 console.log('connected to room', room);
 
 // set up audio track
@@ -30,3 +38,5 @@ const buffer = new Uint16Array(sample.buffer);
 
 await room.localParticipant.publishTrack(track, options);
 await source.captureFrame(new AudioFrame(buffer, 16000, 1, buffer.byteLength / 2))
+
+await room.disconnect();

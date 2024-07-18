34bc3d79d04dba69d69e1109c6db9fd1787138c7
lukasIO <mail@lukasseiler.de>
2024-07-18 12:30:58 +0200

    reuse

--- packages/livekit-rtc/src/utils.ts
+++ packages/livekit-rtc/src/utils.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 LiveKit, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 export function diffAttributes(
   oldValues: Record<string, string> | undefined,
   newValues: Record<string, string> | undefined,

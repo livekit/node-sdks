99abc28e0441daa11da1b572ce310ce897917b87
aoife cassidy <aoife@livekit.io>
2024-09-18 14:35:49 -0700

    add queueing to audiosource

--- packages/livekit-rtc/src/audio_source.ts
+++ packages/livekit-rtc/src/audio_source.ts
@@ -9,31 +9,45 @@ import type {
   CaptureAudioFrameCallback,
   CaptureAudioFrameResponse,
   NewAudioSourceResponse,
-} from './proto/audio_frame_pb.js';
+
+  ClearAudioBufferResponse} from './proto/audio_frame_pb.js';
 import {
   AudioSourceType,
   CaptureAudioFrameRequest,
+  ClearAudioBufferRequest,
   NewAudioSourceRequest,
 } from './proto/audio_frame_pb.js';
+import { Queue } from './utils.js';
 
 export class AudioSource {
   /** @internal */
   info: AudioSourceInfo;
   /** @internal */
   ffiHandle: FfiHandle;
+  /** @internal */
+  lastCapture: number;
+  /** @internal */
+  currentQueueSize: number;
+  /** @internal */
+  releaseQueue = new Queue<void>();
 
   sampleRate: number;
   numChannels: number;
+  queueSize: number;
 
-  constructor(sampleRate: number, numChannels: number, enableQueue?: boolean) {
+  constructor(sampleRate: number, numChannels: number, queueSize = 1000) {
     this.sampleRate = sampleRate;
     this.numChannels = numChannels;
+    this.queueSize = queueSize;
+
+    this.lastCapture = 0;
+    this.currentQueueSize = 0;
 
     const req = new NewAudioSourceRequest({
       type: AudioSourceType.AUDIO_SOURCE_NATIVE,
       sampleRate: sampleRate,
       numChannels: numChannels,
-      enableQueue: enableQueue,
+      queueSizeMs: queueSize,
     });
 
     const res = FfiClient.instance.request<NewAudioSourceResponse>({
@@ -47,7 +61,43 @@ export class AudioSource {
     this.ffiHandle = new FfiHandle(res.source.handle.id);
   }
 
+  get queuedDuration(): number {
+    return Math.max(this.currentQueueSize - Date.now() + this.lastCapture, 0);
+  }
+
+  clearQueue() {
+    const req = new ClearAudioBufferRequest({
+      sourceHandle: this.ffiHandle.handle,
+    });
+
+    FfiClient.instance.request<ClearAudioBufferResponse>({
+      message: {
+        case: 'clearAudioBuffer',
+        value: req,
+      },
+    });
+
+    this.releaseQueue.put();
+  }
+
+  async waitForPlayout() {
+    await this.releaseQueue.get().then(() => {
+      this.lastCapture = 0
+      this.currentQueueSize = 0;
+    })
+  }
+
   async captureFrame(frame: AudioFrame) {
+    const now = Date.now()
+    const elapsed = this.lastCapture === 0 ? 0 : now - this.lastCapture;
+    this.currentQueueSize += frame.samplesPerChannel / frame.sampleRate - elapsed
+
+    // remove 50ms to account for processing time (e.g. using wait_for_playout for very small chunks)
+    this.currentQueueSize -= 0.05
+    this.lastCapture = now
+
+    setTimeout(this.releaseQueue.put, this.currentQueueSize)
+    
     const req = new CaptureAudioFrameRequest({
       sourceHandle: this.ffiHandle.handle,
       buffer: frame.protoInfo(),

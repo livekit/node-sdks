78404433e91edf4655b54e55b0ee85747cf08556
aoife cassidy <aoife@livekit.io>
2024-11-27 20:02:37 +0200

    don't bundle files

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,43 @@
 import { Options } from 'tsup';
 
 const defaultOptions: Options = {
-  entry: ['src/index.ts'],
+  entry: ['src/**/*.ts'],
   format: ['cjs', 'esm'],
   splitting: false,
   sourcemap: true,
   // for the type maps to work, we use tsc's declaration-only command
   dts: false,
   clean: true,
+  bundle: false,
   target: 'node16',
   esbuildOptions: (options, context) => {
     if (context.format === 'esm') {
       options.packages = 'external';
     }
   },
+  plugins: [
+    {
+      // https://github.com/egoist/tsup/issues/953#issuecomment-2294998890
+      // ensuring that all local requires/imports in `.cjs` files import from `.cjs` files.
+      // require('./path') → require('./path.cjs') in `.cjs` files
+      // require('../path') → require('../path.cjs') in `.cjs` files
+      // from './path' → from './path.cjs' in `.cjs` files
+      // from '../path' → from '../path.cjs' in `.cjs` files
+      name: 'fix-cjs-imports',
+      renderChunk(code) {
+        if (this.format === 'cjs') {
+          const regexCjs = /require\((?<quote>['"])(?<import>\.[^'"]+)\.js['"]\)/g;
+          const regexDynamic = /import\((?<quote>['"])(?<import>\.[^'"]+)\.js['"]\)/g;
+          const regexEsm = /from(?<space>[\s]*)(?<quote>['"])(?<import>\.[^'"]+)\.js['"]/g;
+          return {
+            code: code
+              .replace(regexCjs, 'require($<quote>$<import>.cjs$<quote>)')
+              .replace(regexDynamic, 'import($<quote>$<import>.cjs$<quote>)')
+              .replace(regexEsm, 'from$<space>$<quote>$<import>.cjs$<quote>'),
+          };
+        }
+      },
+    },
+  ],
 };
 export default defaultOptions;

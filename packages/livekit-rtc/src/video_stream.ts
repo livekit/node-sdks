d4234ed43db9ce59c0ff812843d9e906ed1f84db
Shubhrakanti Ganguly <shubhrakanti.ganguly@livekit.io>
2025-04-15 13:10:22 -0700

    update video frame

--- packages/livekit-rtc/src/video_stream.ts
+++ packages/livekit-rtc/src/video_stream.ts
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: 2024 LiveKit, Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
-import { Mutex } from '@livekit/mutex';
-import { ReadableStream, TransformStream, TransformStreamDefaultController } from 'node:stream/web';
 import type { FfiEvent } from './ffi_client.js';
 import { FfiClient, FfiClientEvent, FfiHandle } from './ffi_client.js';
 import type {
@@ -13,6 +11,7 @@ import type {
 import { NewVideoStreamRequest, VideoStreamType } from './proto/video_frame_pb.js';
 import type { Track } from './track.js';
 import { VideoFrame } from './video_frame.js';
+import { ReadableStream } from 'node:stream/web';
 
 export type VideoFrameEvent = {
   frame: VideoFrame;
@@ -20,21 +19,22 @@ export type VideoFrameEvent = {
   rotation: VideoRotation;
 };
 
-export class VideoStream implements AsyncIterableIterator<VideoFrameEvent> {
+export class VideoStream extends ReadableStream<VideoFrameEvent> {
   /** @internal */
   info?: VideoStreamInfo;
   /** @internal */
   ffiHandle: FfiHandle;
   /** @internal */
-  private reader: ReadableStreamDefaultReader<VideoFrameEvent>;
-  /** @internal */
-  private onEvent: ((ev: FfiEvent) => void) | null = null;
-  /** @internal */
-  mutex = new Mutex();
+  controller?: ReadableStreamDefaultController<VideoFrameEvent>;
 
   track: Track;
 
-  constructor(track: Track, capacity: number = 0) {
+  constructor(track: Track) {
+    super({
+      start: (controller) => {
+        this.controller = controller;
+      },
+    });
     this.track = track;
 
     const req = new NewVideoStreamRequest({
@@ -52,94 +52,53 @@ export class VideoStream implements AsyncIterableIterator<VideoFrameEvent> {
     this.info = res.stream?.info;
     this.ffiHandle = new FfiHandle(res.stream!.handle!.id!);
 
-    const infinite_capacity = capacity <= 0;
-
-    const source = new ReadableStream<FfiEvent>({
-      start: (controller) => {
-        this.onEvent = (ev: FfiEvent) => {
-          if (
-            ev.message.case === 'videoStreamEvent' &&
-            ev.message.value.streamHandle === this.ffiHandle.handle
-          ) {
-            if (infinite_capacity || (controller.desiredSize && controller.desiredSize > 0)) {
-              controller.enqueue(ev);
-            } else {
-              console.warn('Video stream buffer is full, dropping frame');
-            }
-          }
-        };
-        FfiClient.instance.on(FfiClientEvent.FfiEvent, this.onEvent);
-      },
-      cancel: () => {
-        if (this.onEvent) {
-          FfiClient.instance.off(FfiClientEvent.FfiEvent, this.onEvent);
-          this.onEvent = null;
-        }
-      },
-    });
-
-    const transformStream = new TransformStream<FfiEvent, VideoFrameEvent>(
-      {
-        transform: (
-          event: FfiEvent,
-          controller: TransformStreamDefaultController<VideoFrameEvent>,
-        ) => {
-          if (
-            event.message.case !== 'videoStreamEvent' ||
-            event.message.value.streamHandle !== this.ffiHandle.handle
-          ) {
-            return;
-          }
-
-          const streamEvent = event.message.value.message;
-          switch (streamEvent.case) {
-            case 'frameReceived':
-              const rotation = streamEvent.value.rotation;
-              const timestampUs = streamEvent.value.timestampUs;
-              const frame = VideoFrame.fromOwnedInfo(streamEvent.value.buffer!);
-              controller.enqueue({
-                rotation: rotation!,
-                timestampUs: timestampUs!,
-                frame,
-              });
-              break;
-            case 'eos':
-              controller.terminate();
-              if (this.onEvent) {
-                FfiClient.instance.off(FfiClientEvent.FfiEvent, this.onEvent);
-                this.onEvent = null;
-              }
-              break;
-          }
-        },
-      },
-      {
-        highWaterMark: capacity > 0 ? capacity : undefined,
-      },
-    );
-
-    this.reader = source.pipeThrough(transformStream).getReader();
+    FfiClient.instance.on(FfiClientEvent.FfiEvent, this.onEvent);
   }
 
-  async next(): Promise<IteratorResult<VideoFrameEvent>> {
-    const unlock = await this.mutex.lock();
-    try {
-      const result = await this.reader.read();
-      return {
-        done: result.done,
-        value: result.done ? (undefined as any) : result.value,
-      };
-    } finally {
-      unlock();
+  private onEvent = (ev: FfiEvent) => {
+    if (!this.controller) {
+      throw new Error('Stream controller not initialized');
     }
-  }
+
+    if (
+      ev.message.case != 'videoStreamEvent' ||
+      ev.message.value.streamHandle != this.ffiHandle.handle
+    ) {
+      return;
+    }
+
+    const streamEvent = ev.message.value.message;
+    switch (streamEvent.case) {
+      case 'frameReceived':
+        const rotation = streamEvent.value.rotation;
+        const timestampUs = streamEvent.value.timestampUs;
+        const frame = VideoFrame.fromOwnedInfo(streamEvent.value.buffer!);
+        const value = { rotation, timestampUs, frame };
+        const videoFrameEvent = {
+          frame: value.frame,
+          timestampUs: value.timestampUs!,
+          rotation: value.rotation!,
+        };
+
+        this.controller.enqueue(videoFrameEvent);
+        break;
+      case 'eos':
+        FfiClient.instance.off(FfiClientEvent.FfiEvent, this.onEvent);
+        this.controller.close();
+        break;
+    }
+  };
 
   close() {
-    this.reader.cancel();
+    if (!this.controller) {
+      throw new Error('Stream controller not initialized');
+    }
+      try {
+        this.controller.close();
+      } catch (e) {
+        // Controller might already be closed
+        throw e;
+      }
     this.ffiHandle.dispose();
-  }
-
-  [Symbol.asyncIterator](): VideoStream {
-    return this;
   }
 }

dabbf25348cf65047347a2282686f8e01dd23a6a
lukasIO <mail@lukasseiler.de>
2026-02-12 17:22:29 +0100

    lint

--- packages/livekit-rtc/src/audio_stream.ts
+++ packages/livekit-rtc/src/audio_stream.ts
@@ -5,12 +5,7 @@ import type { UnderlyingSource } from 'node:stream/web';
 import { AudioFrame } from './audio_frame.js';
 import type { FfiEvent } from './ffi_client.js';
 import { FfiClient, FfiClientEvent, FfiHandle } from './ffi_client.js';
-import {
-  FrameProcessor,
-  FrameProcessorSymbol,
-  isAudioFrameProcessor,
-  isFrameProcessor,
-} from './frame_processor.js';
+import { type FrameProcessor, isAudioFrameProcessor } from './frame_processor.js';
 import { log } from './log.js';
 import type { NewAudioStreamResponse } from './proto/audio_frame_pb.js';
 import { AudioStreamType, NewAudioStreamRequest } from './proto/audio_frame_pb.js';

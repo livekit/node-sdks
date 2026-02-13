4ad9892f850588b0f566a5646bfe0a682279a636
lukasIO <mail@lukasseiler.de>
2026-02-13 15:14:22 +0100

    fix build

--- packages/livekit-rtc/src/audio_stream.ts
+++ packages/livekit-rtc/src/audio_stream.ts
@@ -5,7 +5,7 @@ import type { UnderlyingSource } from 'node:stream/web';
 import { AudioFrame } from './audio_frame.js';
 import type { FfiEvent } from './ffi_client.js';
 import { FfiClient, FfiClientEvent, FfiHandle } from './ffi_client.js';
-import { type FrameProcessor, isAudioFrameProcessor } from './frame_processor.js';
+import { type FrameProcessor, isFrameProcessor } from './frame_processor.js';
 import { log } from './log.js';
 import type { NewAudioStreamResponse } from './proto/audio_frame_pb.js';
 import { AudioStreamType, NewAudioStreamRequest } from './proto/audio_frame_pb.js';
@@ -41,7 +41,7 @@ class AudioStreamSource implements UnderlyingSource<AudioFrame> {
     if (sampleRateOrOptions !== undefined && typeof sampleRateOrOptions !== 'number') {
       this.sampleRate = sampleRateOrOptions.sampleRate ?? 48000;
       this.numChannels = sampleRateOrOptions.numChannels ?? 1;
-      if (isAudioFrameProcessor(sampleRateOrOptions.noiseCancellation)) {
+      if (isFrameProcessor(sampleRateOrOptions.noiseCancellation)) {
         this.frameProcessor = sampleRateOrOptions.noiseCancellation;
       } else {
         this.legacyNcOptions = sampleRateOrOptions.noiseCancellation;
